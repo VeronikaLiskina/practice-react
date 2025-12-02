baa6367ad54f42dab5cb61ad33c8357a480cca0c
VeronikaLiskina <142492563+VeronikaLiskina@users.noreply.github.com>
2025-12-02 21:07:15 +0800

    переход на vite 1

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // для React
+// или import vue from '@vitejs/plugin-vue' // для Vue
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    react(), // или vue()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+})
